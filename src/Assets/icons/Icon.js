aa50d0daa33d8dcb1484dfb75ea7864140e21e82
tobby8629 <sarhmullah15@gmail.com>
2024-10-15 16:28:14 -0500

    Image and Icons setup

---
+++ src/Assets/icons/Icon.js
@@ -0,0 +1,27 @@
+import arrowLeft from './arrowLeft.png'
+import arrowRight from './arrowRight.png'
+import facebook from './facebook.png'
+import instagram from './instagram.png'
+import linkedIn from './linkedIn.png'
+import mail from './mail.png'
+import menu from './menu.png'
+import phone from './phone.png'
+import quotes from './quotes.png'
+import search from './search.png'
+import times from './times.png'
+import twitter from './twitter.png'
+
+export default Icons = {
+  arrowLeft: arrowLeft,
+  arrowRight,
+  facebook,
+  instagram,
+  linkedIn,
+  mail,
+  menu,
+  phone,
+  quotes,
+  search,
+  times,
+  twitter
+} 
---
+++ src/Assets/images/Images.js
@@ -0,0 +1,19 @@
+import alex from  './alex.png'
+import angea from  './angea.png'
+import banner_section from  './banner_section.png'
+import header1 from  './header1.png'
+import header2 from  './header2.png'
+import header3 from  './header3.png'
+import logo from  './logo.png'
+import tom from  './tom.png'
+
+export default Images = {
+  alex,
+  angea,
+  banner_section,
+  header1,
+  header2,
+  header3,
+  logo,
+  tom
+}
